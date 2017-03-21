7facb9b52732d25801056c118dcd8b800e25803e
dernori <dernori@gmail.com>
2017-03-21 13:05:43 +0100

    initial commit

---
+++ logiceval/src/test/java/logiceval/DefaultTest.java
@@ -0,0 +1,11 @@
+package logiceval;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class DefaultTest {
+
+        @Test
+        public void dummyTest() {
+        	assertTrue(true);}
+}
