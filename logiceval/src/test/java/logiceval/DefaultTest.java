044c920027f436a94c34dbd0dfca506ae96fac1a
ThreeBL Tamas <harem987@gmail.com>
2017-05-11 20:03:04 +0200

    Git configured and working, first commit to test it how it works. Add comment to dummy test class.

--- logiceval/src/test/java/logiceval/DefaultTest.java
+++ logiceval/src/test/java/logiceval/DefaultTest.java
@@ -3,6 +3,10 @@ package logiceval;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+/**
+ * Created by ThreeBL Tamas on 2017.05.11..
+ * Dummy comment.
+ */
 public class DefaultTest {
 
 	@Test
